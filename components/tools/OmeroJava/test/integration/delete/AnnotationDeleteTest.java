a581116fafb1793d38e44d0f768ac7d70b6924a2
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2014-11-07 12:39:16 +0000

    annotations on deleted annotations should be deleted
    see comment regarding trac ticket 5793 in
    AbstractHierarchyGraphSpec.queryBackupIds

--- components/tools/OmeroJava/test/integration/delete/AnnotationDeleteTest.java
+++ components/tools/OmeroJava/test/integration/delete/AnnotationDeleteTest.java
@@ -243,8 +243,8 @@ public class AnnotationDeleteTest extends AbstractServerTest {
         newUserAndGroup("rw----");
         Annotation ann = (Annotation) iUpdate
                 .saveAndReturnObject(new TagAnnotationI());
-        annotateSaveDeleteAndCheck(ann, DeleteServiceTest.REF_ANN, ann.getId(),
-                false);
+        annotateSaveDeleteAndCheck(ann, DeleteServiceTest.REF_ANN,
+                ann.getId());
     }
 
     /**
